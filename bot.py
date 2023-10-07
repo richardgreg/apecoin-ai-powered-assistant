26696f1fe3810139ee1e67d0db4b1b06f79a10d7
Richard Gregory <richardgrecoson@gmail.com>
2023-10-07 23:03:38 +0100

    Update prompt template

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv, find_dotenv
 import os
 from langchain.prompts import SystemMessagePromptTemplate, PromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
@@ -17,7 +17,7 @@ loader = TextLoader("./context.txt")
 documents = loader.load()
 
 # Split our document for easy retrieval
-text_splitter = CharacterTextSplitter(chunk_size=5000)
+text_splitter = RecursiveCharacterTextSplitter(add_start_index=True, chunk_size=5000)
 texts = text_splitter.split_documents(documents)
 
 # Embed and retrieve text
@@ -26,12 +26,15 @@ retriever = Chroma.from_documents(texts, embeddings).as_retriever()
 chat = ChatOpenAI(temperature=0, model="gpt-4")
 
 # Prompt template where we provide context
-prompt_template = """You are a helpful dicord bot.
+prompt_template = """
+You are an AI-powered discord assistant for the ApeCoin DAO community.
+    
+You are designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, you are able to generate human-like text based on the input you receive, allowing you to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
 
+You are constantly learning and improving, and your capabilities are constantly evolving. You are able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. You have access to some personalized information provided by the human in the Context section below. Additionally, you are able to generate your own text based on the input you receive, allowing you to engage in discussions and provide explanations and descriptions on a wide range of topics.
 {context}
-
-Please provide the most suitable response for the users question.
-Answer:"""
+Overall, you are a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether the human needs help with a specific question or just wants to have a conversation about a particular topic, you are here to assist.
+"""
 
 # Prepare prompt template for OpenAI
 prompt = PromptTemplate(
@@ -69,4 +72,4 @@ async def apegpt(ctx, *, question):
         await ctx.send("Sorry, I was unable to process your question.")
 
 
-# bot.run(os.environ.get("DISCORD_TOKEN"))
+bot.run(os.environ.get("DISCORD_TOKEN"))
